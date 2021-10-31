aa62f8f9f1b0a0b4bd9941031c1b81897f4aff0b
Vsevolod <polonsky123@bk.ru>
2021-10-31 16:26:10 +0300

    login error contine

--- profile.php
+++ profile.php
@@ -12,36 +12,39 @@
 
 	<!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
 		
+	<!-- загрузка файлов методом Post 
+		следует убедиться, что форма загрузки имеет атрибут enctype="multipart/form-data" , в противном случае загрузка файлов на сервер не произойдёт. -->
 	<form enctype="multipart/form-data" action="" method="POST" class="profileForm">
 	    <div class="profileFormBlock">
-	    	<img class="profileImg" src="">
+	    	<img class="profileImg" src="https://99px.ru/sstorage/56/2018/02/image_56180218112942267869.jpg">
 		  	<input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
 		   <input type="file" name="avatarImage">
 	    </div>
-		    <input type="submit" value="Отправить файл" />
-	</form>
-	
-
-	<!-- загрузка файлов методом Post 
-		следует убедиться, что форма загрузки имеет атрибут enctype="multipart/form-data" , в противном случае загрузка файлов на сервер не произойдёт. -->
-	<!-- <form enctype="multipart/form-data" action="" method="POST" class="profileForm">
-		<div class="profileFormBlock">
-			<img class="profileImg" src="">
-			<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-			<input type="file" name="avatarImage">
-		</div>
 		<p class="profileTxtLogin"></p>
 		<textarea type="text" name="selfDescription" placeholder="Расскажите о себе"></textarea>	
 		<input type="submit" value="Изменить" name="bt">
+	</form>
+	
+
+	<!-- 
 	</form>  -->
+
 <!-- обеспечиваем процесс загрузки -->
 
 
 <?php
+// куда загрузить
 $uploaddir = 'img/';
+/*
+$uploadfile состоит из  $uploaddir, который связыватеся с basename
+	basename - специальная функция которая вытаскивает назавние этого файла
+	avatarImage - userName взято из атрибута input
+	name - имя файла которое храниться непосредственно у клиента. (зарезервировано не меняем)
+*/
 $uploadfile = $uploaddir . basename($_FILES['avatarImage']['name']);
-// phpinfo();
+
 echo '<pre>';
+// проверка загрузиться ли файл и если так, то он перекидывается в директорию с тем названием, которое вытащила функция  basename
 if (move_uploaded_file($_FILES['avatarImage']['tmp_name'], $uploadfile)) {
     echo "Файл корректен и был успешно загружен.\n";
 } else {
