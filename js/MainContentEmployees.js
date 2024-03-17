45a191e2331ff8a7a7665b491e4ff601f4fb7237
vpolonsky55 <kartraid@mail.ru>
2024-03-17 15:34:10 +0300

    до создания файловой структуры папок MVC

--- js/MainContentEmployees.js
+++ js/MainContentEmployees.js
@@ -1,19 +1,29 @@
+
+
 class MainContentEmployees extends Form
 {
 	constructor(...args)
 	{
 		super(...args)
-		this.btnAdd = new Button(this.obj, ".main-content__btn_add", "button")
-		this.btnAdd.setValue("Добавить")
-		this.btnAdd.obj.addEventListener("click", function(event)
+		this.buttonAddParent = this.obj
+		this.buttonAddCssClass = ".main-content__btn_add"
+		this.buttonAddHtmlInputType = "button"
+		this.buttonAddText = "Добавить"
+
+		this.buttonAdd = new Button(this.buttonAddParent, this.buttonAddCssClass, this.buttonAddHtmlInputType)
+		this.buttonAdd.setValue(this.buttonAddText)
+		this.buttonAdd.obj.addEventListener("click", function(event)
 			{
 				this.emplModal = new ModalAddEmpl(document.body, ".modal-admin")
 			}
 		)
-		this.employees = new Div(this.obj, ".main-content__employees")
+
+		this.employeesWraperParent = this.obj
+		this.employeesWraperCssClass = ".main-content__employees"
+		this.employeesWraper = new Div(employeesWraperParent, this.employeesWraperCssClass)
 		this.employeesRows = []
-		this.eploysList =  send("POST", 'http://localhost/chat/admin/admin_manager_users.php', "getAllEmploys=1");
-		this.eploysList.then( (emloyes) =>
+		this.eployeesGroup =  send("POST", 'http://localhost/chat/admin/admin_manager_users.php', "getAllEmploys=1");
+		this.eployeesGroup.then( (emloyes) =>
 			{
 				emloyes.forEach(value => 
 					{
@@ -21,11 +31,12 @@ class MainContentEmployees extends Form
 					}
 				)
 			}
+			
 		)
 	}
 	addEmploye(name, email, department, id)
 	{
-		this.employeesRows.push(new EmployeesRow(this.employees.obj, ".main-content__employees-row"))
+		this.employeesRows.push(new EmployeesRow(this.employeesWraper.obj, ".main-content__employees-row"))
 		this.employeesRows[this.employeesRows.length - 1].setValue(name, email, department, id)
 	}
 }
